4a580e2377678ded7f2d431ae0caa23f5b86e7bf
Smill Vásquez <smillalexander@gmail.com>
2016-09-19 22:52:53 -0400

    was implemented angular module productCatApp in index view

--- admin/public/javascripts/productCatView.js
+++ admin/public/javascripts/productCatView.js
@@ -1,5 +1,11 @@
 var productCatApp = angular.module('productCatApp',["ui.bootstrap"]);
 
 productCatApp.controller('ProductCatController',function ProductCatController($scope,$http){
+    $scope.products = [];
+    $http.get("/api/products").then(function(result){
+        $scope.products = result.data;
+    },function (err){
+        alert(err);
+    });
 
 });
